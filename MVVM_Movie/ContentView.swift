1b82d865265bca6a38b669fbdfc5fb1a1074cea5
LeeHsss <lii0730@icloud.com>
2022-03-23 15:41:50 +0900

    Initial Commit

---
+++ MVVM_Movie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MVVM_Movie
+//
+//  Created by LeeHsss on 2022/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MVVM_Movie/MVVM_MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_MovieApp.swift
+//  MVVM_Movie
+//
+//  Created by LeeHsss on 2022/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
